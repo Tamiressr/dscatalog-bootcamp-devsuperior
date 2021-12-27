c67457ca48523bc3f89f66b447be57d3fd7fd693
Tamires <tamiressiqueira12gmail.com>
2021-12-27 15:14:05 -0300

    Find Category byid , exception handling

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dscatalog.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dscatalog.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+  @ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError>objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+	  StandardError error= new StandardError();
+	  error.setTimestamp(Instant.now());
+	  error.setStatus(HttpStatus.NOT_FOUND.value());
+	  error.setError("Resource not found");
+	  error.setMessage(e.getMessage());
+	  error.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+}
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -2,8 +2,7 @@ package com.devsuperior.dscatalog.services;
 
 import java.util.Arrays;
 import java.util.List;
-
-
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
@@ -21,5 +21,9 @@ public class CategoryService {
 	public List<CategoryDTO>findAll(){
 		List<Category>list=repository.findAll();
 		return  list.stream().map(c->new CategoryDTO(c)).toList();//função map transforma um elemento original em outra coisa
+	}
+	public CategoryDTO findById(Long id) {
+		Optional<Category> objDTO= repository.findById(id);
+		return new CategoryDTO(objDTO.orElseThrow(()->new ObjectNotFoundException("Categoria não encontrada")));
 	}
 }
